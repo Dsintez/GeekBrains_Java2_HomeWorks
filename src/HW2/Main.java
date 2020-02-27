b158c7b16879f9d68bec77ba9eac5d6b1258b4b5
Dmirtry Sidorov <chaka.00@mail.ru>
2020-02-27 21:28:58 +0300

    Throws + Исправления

--- src/HW2/Main.java
+++ src/HW2/Main.java
@@ -3,11 +3,27 @@ package HW2;
 public class Main {
 
     public static void main(String[] args) {
-        String[][] s = {{"1","2","2","2"},
-                        {"1","2","2","2"},
-                        {"1","2","2","2"},
-                        {"1","2","2","2"}};
+        String[][] CorrectArr = {{"1","2","2","2"},
+                                 {"1","2","2","2"},
+                                 {"1","2","2","2"},
+                                 {"1","2","2","2"}};
 
+        String[][] InCorrectArr1 = {{"1","2","2"},
+                                    {"1","2","2","2"},
+                                    {"1","2","2","2"},
+                                    {"1","2","2","2"}};
+
+        String[][] InCorrectArr2 = {{"1","2","2","T"},
+                                    {"1","2","2","2"},
+                                    {"1","2","2","2"},
+                                    {"1","2","2","2"}};
+
+        sum(CorrectArr);
+        sum(InCorrectArr1);
+        sum(InCorrectArr2);
+    }
+
+    private static void sum(String[][] s) {
         try {
             System.out.println(Math.sumArr(s));
         } catch (MyArraySizeException e) {
--- src/HW2/Math.java
+++ src/HW2/Math.java
@@ -1,26 +1,30 @@
 package HW2;
 
 public class Math {
-    public static int sumArr(String[][] array) throws MyArraySizeException {
-        int size = 4;
-        int summa = 0;
-        if (array.length == size){
-            for (int i = 0; i < size; i++) {
-                if (array[i].length != size) throw new MyArraySizeException("" + i + " array invalid length");
-            }
-        } else {
-            throw new MyArraySizeException("main array invalid length");
-        }
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+    private final static int SIZE = 4;
+    public static int sumArr(String[][] array) throws MyArraySizeException {
+        checkArraySize(array);
+        int sum = 0;
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
                 try {
-                    summa = summa + Integer.parseInt(array[i][j]);
+                    sum += Integer.parseInt(array[i][j]);
                 } catch (NumberFormatException e){
                     throw new MyArrayDataException(i, j);
                 }
             }
         }
-        return summa;
+        return sum;
+    }
+
+    private static void checkArraySize(String[][] array) {
+        if (array.length == SIZE){
+            for (int i = 0; i < SIZE; i++) {
+                if (array[i].length != SIZE) throw new MyArraySizeException("" + i + " array invalid length");
+            }
+        } else {
+            throw new MyArraySizeException("main array invalid length");
+        }
     }
 }
